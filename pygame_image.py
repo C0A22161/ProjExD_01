cd7fc8d54b67f9398248e5332b2b38f2c1f64f6a
kihiro <c0a2216112@edu.teu.ac.jp>
2023-11-21 15:40:30 +0900

    練習3

--- pygame_image.py
+++ pygame_image.py
@@ -8,7 +8,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01proen/fig/pg_bg.jpg")#背景画像練習1
     bg_img2 = pg.image.load("ex01proen/fig/3.png")
-    pg.transform(bg_img2,True,False)
+    bg_img2 = pg.transform.flip(bg_img2,True,False)
+    bg_img3 = [bg_img, pg.transform.rotozoom(bg_img2, 30, 1.0)]#練習3こうかとんのSurfaceのリスト
 
     tmr = 0
     while True:
@@ -16,7 +17,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(bg_img2,[300,200])
+        screen.blit(bg_img2[1],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
