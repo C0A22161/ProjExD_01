4a683b6b7a1fbd1b978be2b12607cbe580e35b32
kihiro <c0a2216112@edu.teu.ac.jp>
2023-11-21 15:33:14 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01proen/fig/pg_bg.jpg")#背景画像練習1
+    bg_img2 = pg.image.load("ex01proen/fig/3.png")
+    pg.transform(bg_img2,True,False)
+
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img2,[300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
